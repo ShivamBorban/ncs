9d1b7c647b2db0df27d946444f1f7954e6e168ff
Shivam Borban <shivamborban14321@gmail.com>
2024-12-05 19:23:40 +0530

    commited

--- file.sh
+++ file.sh
@@ -3,6 +3,8 @@
 num1=$1
 num2=$2
 num3=$3
+num4=$4
+#############################################################
 
 echo " "
 echo "1st Number is = ${num1}"
@@ -14,16 +16,30 @@ echo "Subtraction = $((num1-num2))"
 echo "Multiplication = $((num1*num2))"
 echo "Quotient = $((num1/num2))"
 echo "Reminder = $((num1%num2))"
+##############################################################
 
 echo " "
-
 echo "Check ${num3} is EVEN or ODD ? "
+
 echo " "
 if [ $((num3 % 2)) -eq 0 ]; then
     echo "${num3} is a EVEN NUMBER"
 else
     echo "${num3} is a ODD NUMBER"
 fi
+
+################################################################
+
+echo " "
+echo "Multiplication Table for ${num4} :"
+for i in {1..10}
+do
+    echo "${num4} x $i = $((num4 * i))"
+done
+
+
+
+echo " "
 
 
 
